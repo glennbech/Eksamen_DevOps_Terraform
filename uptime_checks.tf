6b7670c34fb824631952b321aef071a4cb3f8081
joakimstolen <joakimstoelen@hotmail.com>
2020-11-17 15:56:28 +0100

    Added statuscake api key and implementation

---
+++ uptime_checks.tf
@@ -0,0 +1,11 @@
+provider "statuscake" {
+  username = "joakimstoelenhotmailcom"
+}
+
+resource "statuscake_test" "google" {
+  test_type = "HTTP"
+  website_name = "DevOps Eksamen"
+  website_url = google_cloud_run_service.default.status[0].url
+  check_rate = 10
+  contact_group = ["193690"]
+}
