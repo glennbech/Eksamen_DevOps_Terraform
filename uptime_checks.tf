5c3dcdd16c56ff784fecd7cb32ac16cf10a7ba6a
joakimstolen <joakimstoelen@hotmail.com>
2020-11-17 17:56:36 +0100

    Added newest docker image 3.0

--- uptime_checks.tf
+++ uptime_checks.tf
@@ -6,7 +6,7 @@ resource "statuscake_test" "google" {
   test_type = "HTTP"
   website_name = "DevOps Eksamen"
   website_url = google_cloud_run_service.default.status[0].url
-  check_rate = 10
+  check_rate = 300
   contact_group = ["193689"]
 
 
