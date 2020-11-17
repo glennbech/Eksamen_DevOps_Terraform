5af8cbf26c36096169ed90f72ffdf713ca8f03aa
joakimstolen <joakimstoelen@hotmail.com>
2020-11-17 16:23:13 +0100

    checking if contact group will be contacted

--- uptime_checks.tf
+++ uptime_checks.tf
@@ -5,7 +5,7 @@ provider "statuscake" {
 resource "statuscake_test" "google" {
   test_type = "HTTP"
   website_name = "DevOps Eksamen"
-  website_url = google_cloud_run_service.default.status[0].url
+  website_url = "www.thisdoesnotexisthopefully123.com"
   check_rate = 10
   contact_group = ["195037"]
 }
