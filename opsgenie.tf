6fc4f2929b2166be4e5d1b9115630a574d98b21f
joakimstolen <joakimstoelen@hotmail.com>
2020-11-19 11:33:26 +0100

    Trying to get users authenticated 2.0

--- opsgenie.tf
+++ opsgenie.tf
@@ -2,59 +2,62 @@ provider "opsgenie" {
   api_key = "6c150a28-735a-4804-a9f9-8925f8875ab6"
   api_url = "api.eu.opsgenie.com"
 }
-data "opsgenie_user" "opsgenie_owner" {
-  username = var.opsgenie_owner_email
-}
-
-
-resource "opsgenie_user" "userEksamenOne" {
-  full_name = "Test User"
-  role = "User"
-  username = "test@user.abc"
-  locale    = "no_NO"
-  timezone  = "Europe/Oslo"
-}
-
-resource "opsgenie_user" "userEksamenTwo" {
-  full_name = "Another User"
-  role = "User"
-  username = "another@user.abc"
-  locale    = "no_NO"
-  timezone  = "Europe/Oslo"
-}
 
 
 
-resource "opsgenie_team" "testTeam" {
-  name = "Team Test"
-
-  member {
-    id = opsgenie_user.userEksamenOne.id
-    role = "user"
-  }
-
-  member {
-    id = opsgenie_user.userEksamenTwo.id
-    role = "user"
-  }
-}
-
-resource "opsgenie_schedule" "mainschedule" {
-  name = "main_schedule"
-  owner_team_id = opsgenie_team.testTeam.id
-}
-
-resource "opsgenie_schedule_rotation" "team1rotation" {
-  schedule_id = opsgenie_schedule.mainschedule.id
-  start_date = "2020-11-20T00:00:00Z"
-  type = "hourly"
-  participant {
-    type = "user"
-    id = opsgenie_user.userEksamenOne.id
-  }
-
-  participant {
-    type = "user"
-    id = opsgenie_user.userEksamenTwo.id
-  }
-}
+//data "opsgenie_user" "opsgenie_owner" {
+//  username = var.opsgenie_owner_email
+//}
+//
+//
+//resource "opsgenie_user" "userEksamenOne" {
+//  full_name = "Test User"
+//  role = "User"
+//  username = "test@user.abc"
+//  locale    = "no_NO"
+//  timezone  = "Europe/Oslo"
+//}
+//
+//resource "opsgenie_user" "userEksamenTwo" {
+//  full_name = "Another User"
+//  role = "User"
+//  username = "another@user.abc"
+//  locale    = "no_NO"
+//  timezone  = "Europe/Oslo"
+//}
+//
+//
+//
+//resource "opsgenie_team" "testTeam" {
+//  name = "Team Test"
+//
+//  member {
+//    id = opsgenie_user.userEksamenOne.id
+//    role = "user"
+//  }
+//
+//  member {
+//    id = opsgenie_user.userEksamenTwo.id
+//    role = "user"
+//  }
+//}
+//
+//resource "opsgenie_schedule" "mainschedule" {
+//  name = "main_schedule"
+//  owner_team_id = opsgenie_team.testTeam.id
+//}
+//
+//resource "opsgenie_schedule_rotation" "team1rotation" {
+//  schedule_id = opsgenie_schedule.mainschedule.id
+//  start_date = "2020-11-20T00:00:00Z"
+//  type = "hourly"
+//  participant {
+//    type = "user"
+//    id = opsgenie_user.userEksamenOne.id
+//  }
+//
+//  participant {
+//    type = "user"
+//    id = opsgenie_user.userEksamenTwo.id
+//  }
+//}
