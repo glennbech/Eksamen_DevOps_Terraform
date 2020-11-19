5a12d7e239c2bd87041e2070d07d5b85586b1d63
joakimstolen <joakimstoelen@hotmail.com>
2020-11-19 11:00:33 +0100

    Trying to make opsgenie work

--- opsgenie.tf
+++ opsgenie.tf
@@ -32,3 +32,23 @@ resource "opsgenie_team" "testTeam" {
     role = "user"
   }
 }
+
+resource "opsgenie_schedule" "mainschedule" {
+  name = "main_schedule"
+  owner_team_id = opsgenie_team.testTeam.id
+}
+
+resource "opsgenie_schedule_rotation" "team1rotation" {
+  schedule_id = opsgenie_schedule.mainschedule.id
+  start_date = "2020-11-20T00:00:00Z"
+  type = "hourly"
+  participant {
+    type = "user"
+    id = opsgenie_user.user1.id
+  }
+
+  participant {
+    type = "user"
+    id = opsgenie_user.user2.id
+  }
+}
